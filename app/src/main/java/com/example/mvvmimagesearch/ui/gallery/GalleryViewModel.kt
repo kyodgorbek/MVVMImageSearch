a7c7095302689727b7e57f05281b23868e1b2c08
kyodgorbek@gmail.com <adilnoz1992>
2021-04-13 09:22:13 +0200

    added DetailsFragment.kt, SavedStateHandle and finished the Project

--- app/src/main/java/com/example/mvvmimagesearch/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/mvvmimagesearch/ui/gallery/GalleryViewModel.kt
@@ -1,28 +1,31 @@
 package com.example.mvvmimagesearch.ui.gallery
 
-import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import com.example.mvvmimagesearch.data.UnsplashRepository
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel
-@Inject constructor(private val repository: UnsplashRepository):ViewModel() {
+@Inject constructor(private val repository: UnsplashRepository,
+                    @Assisted state: SavedStateHandle) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
+
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString)
 
     }
 
-    fun searchPhotos(query:String){
+    fun searchPhotos(query: String) {
         currentQuery.value = query
     }
 
-    companion object{
+    companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 
--- app/src/main/java/com/example/mvvmimagesearch/ui/gallery/UnsplashPhotoAdapter.kt
+++ app/src/main/java/com/example/mvvmimagesearch/ui/gallery/UnsplashPhotoAdapter.kt
@@ -11,7 +11,7 @@ import com.example.mvvmimagesearch.R
 import com.example.mvvmimagesearch.data.UnsplashPhoto
 import com.example.mvvmimagesearch.databinding.ItemUnsplashPhotoBinding
 
-class UnsplashPhotoAdapter:PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.PhotoViewHolder>(
+class UnsplashPhotoAdapter(private val listener:OnItemClickListener):PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.PhotoViewHolder>(
     PHOTO_COMPARATOR) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
@@ -28,8 +28,24 @@ class UnsplashPhotoAdapter:PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter
     }
 
 
-    class PhotoViewHolder(private val binding: ItemUnsplashPhotoBinding) :
+    inner class PhotoViewHolder(private val binding: ItemUnsplashPhotoBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+       init {
+           binding.root.setOnClickListener {
+            val position = bindingAdapterPosition
+             if(position != RecyclerView.NO_POSITION){
+                 val item = getItem(position)
+                 if(item != null){
+                     listener.onItemClick(item)
+                 }
+             }
+
+
+
+           }
+       }
+
         fun bind(photo: UnsplashPhoto) {
             binding.apply {
                 Glide.with(itemView)
@@ -43,6 +59,9 @@ class UnsplashPhotoAdapter:PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter
         }
     }
 
+    interface OnItemClickListener{
+        fun onItemClick(photo: UnsplashPhoto)
+    }
     companion object {
         private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<UnsplashPhoto>() {
             override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
